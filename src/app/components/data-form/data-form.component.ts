e46898eacf2514425d5fff88f214e0660b33a4e7
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-03-11 10:44:38 +0530

    import CommonModule into data form added pipe

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-data-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './data-form.component.html',
   styleUrl: './data-form.component.css'
 })
