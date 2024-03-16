9b8c5a5931e6e348bfaa79b3e60014e1fe9a37d8
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-03-16 22:16:06 +0530

    created students array

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -12,6 +12,28 @@ import { CommonModule } from '@angular/common';
 export class DataFormComponent {
   value = "";
   today = new Date();
+
+  students = [
+    {
+      id: 1,
+      name: "John",
+      age: 20,
+      gender: 0
+    },
+    {
+      id: 2,
+      name: "Jane",
+      age: 21,
+      gender: 1
+    },
+    {
+      id: 3,
+      name: "Doe",
+      age: 22,
+      gender: 0
+    }
+  ]
+
   clearValue() {
     this.value = "";
   }
