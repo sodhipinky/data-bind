065e05798186005dec64ab787c5895c513237951
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-03-16 22:16:32 +0530

    import GenderPipe

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { GenderPipe } from '../../pipes/gender.pipe';
 
 @Component({
   selector: 'app-data-form',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, GenderPipe],
   templateUrl: './data-form.component.html',
   styleUrl: './data-form.component.css'
 })
